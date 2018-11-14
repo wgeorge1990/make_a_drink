05fd8c0688402c25c37170749f5a0b4c441b85e2
wgeorge1990 <williameverettgeorge@icloud.com>
2018-11-14 17:01:05 -0500

    completed seed.rb

--- bin/run.rb
+++ bin/run.rb
@@ -1,1 +1,7 @@
 require_relative 'config/environment'
+
+
+  def greet
+    puts "Welcome to Bartender, the command line solution that will help you make the best mixed drink on the block!"
+  end
+  greet
